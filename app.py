59ee44c5be9280e39ed53d18b48c9c50f188596d
pollmix <polboy777@gmail.com>
2021-05-09 20:16:38 +0600

    remove span

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+from bs4 import BeautifulSoup
+
+directory = 'bangla'
+
+for filename in os.listdir(directory):
+    if filename.endswith(".html"):
+        print(os.path.join(directory, filename))
+        data = None
+        with open(os.path.join(directory, filename)) as f:
+            data = f.read()
+        with open(os.path.join(directory, filename), 'w') as f:
+            soup = BeautifulSoup(data, features="html.parser")
+            first_section = soup.find('section')
+            span = soup.new_tag('span', attrs={'class': 'play-tips'})
+            span.string = "নিচের ▶ বাটনে ক্লিক করুন"
+            first_section.append(span)
+            f.write(str(soup))
+
+        # print(data)
+    # exit()
