4f34ab32b071dc42e86162a827dd588e1c081a7c
imranpollob <polboy777@gmail.com>
2024-03-24 13:48:02 -0400

    move config to standalone file

--- README.md
+++ README.md
@@ -1,5 +1,26 @@
-## Quran with audio
+## Bangla Quran with audio
 
 Arabic and bangla translation of whole Quran with audio
 
-[quran.imranpollob.com](https://banglaquran.app)
+[banglaquran.app](https://banglaquran.app)
+
+
+## Installation
+```bash
+npm install
+bpm start
+```
+
+Build the app `npm run build`
+
+`dist` folder will be auto generated. But but remove it, because I have added some css and js there.
+
+
+## Next milestones
+- Fix mobile version's layout issue
+- Add logo
+- Add user login feature to track last read location
+- Store guest user tracking to local storage
+- Store zoom settings
+- Replace html default audio control with paly/pause button
+- Add Tafseer
---
+++ dist/reveal-config.js
@@ -0,0 +1,29 @@
+Reveal.initialize({
+  width: "100%",
+  height: "100%",
+  margin: 0.1,
+  minScale: 1,
+  maxScale: 1,
+  touch: false,
+  hash: true,
+  transition: "fade", // none/fade/slide/convex/concave/zoom
+  backgroundTransition: "none", // none/fade/slide/convex/concave/zoom
+  plugins: [RevealAudioSlideshow, RevealMenu],
+  audio: {
+    prefix: "audio/",
+    suffix: ".mp3",
+    advance: 500,
+    autoplay: false,
+    defaultAudios: true, // try to play audios with names such as audio/1.2.ogg
+    playerOpacity: 1,
+  },
+  menu: {
+    custom: [
+      {
+        title: "সূরাসমুহ",
+        icon: '<i class="fa fa-list">',
+        src: "list.html",
+      },
+    ],
+  },
+});
